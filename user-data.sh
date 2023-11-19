f9048bdc2d8c359b4e361bf757d7ed68954bd9d2
AmanUG <45122256+AmanUG@users.noreply.github.com>
2023-11-19 09:49:42 +0000

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-yum update -y
-
 yum install -y httpd
 
 systemctl start httpd
@@ -31,16 +28,91 @@ INSTANCE_REGION=$(printf "$INSTANCE_INFO" |  grep region | awk '{print $3}' | se
 
 cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset=""utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
+<html lang="en">
+<head>
+  <title>Hello World sample page created using EC2 userdata, by Antonio Feijao UK (https://www.antoniofeijao.com/)</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <style>
+    body {
+      background: url("https://www.antoniofeijao.com/assets/images/philipp-katzenberger-iIJrUoeRoCQ-unsplash.jpg") no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+      background-color: DarkSlateGrey;
+      text-align: center;
+      color: GhostWhite;
+    }
+    
+    .centered {
+      position: fixed;
+      top: 20%;
+      left: 50%;
+      /* bring your own prefixes */
+      transform: translate(-50%, -50%);
+    }
+    
+    .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: GhostWhite;
+            color: DarkSlateGrey;
+            text-align: center;
+    }
+    
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 80%;
+      font-size: 1.875em;
+    }
+    
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+  </style>
+</head>
+<body>
+  <div class=centered>
+    <h1>Hello World sample page created using EC2 userdata</h1>
+    <h2>by Antonio Feijao UK</h2>
+    
+    <h3 id="my-url"></h3>
 
-    <body>
-        <img src="https://demo-class-bucket-2023.s3.eu-west-2.amazonaws.com/cloud_43.webp">
+    <table>
+      <tr>
+        <td>Instance ID</td>
+        <td><code> ${INSTANCE_ID} </code></td>
+      </tr>
+      <tr>
+        <td>Instance Type</td>
+        <td><code> ${INSTANCE_TYPE} </code></td>
+      </tr>
+      <tr>
+        <td>Instance Region</td>
+        <td><code> ${INSTANCE_REGION} </code></td>
+      </tr>
+      <tr>
+        <td>Availability Zone</td>
+        <td><code> ${INSTANCE_AZ} </code></td>
+      </tr>
+    </table>
+  </div>
 
-    </body>
+    <div class="footer">
+        <p><a href="https://www.antoniofeijao.com/" target="_blank">https://www.antoniofeijao.com/</a></p>
+    </div>
 
+    <script>
+        document.getElementById("my-url").innerHTML = 
+        "Welcome to " + window.location.href;
+    </script>
+
+</body>
 </html>
 EOF
